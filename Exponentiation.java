a6a38538e256a4efe7d85a8ae4c8a8ba88d14502
Ian Fernandes <34047201+fernandesi2244@users.noreply.github.com>
2019-05-27 13:24:16 -0500

    Update Exponentiation.java with comments

--- Exponentiation.java
+++ Exponentiation.java
@@ -3,7 +3,13 @@ import java.io.*;
 
 /**
  * @author Ian Fernandes
- *
+ * Class represents calculations with ordered pairs of decimal values.
+ *Multiplication: (a,b) * (c,d) = (a*c - b*d, a*d+b*c)
+ *Exponentation is repeated multiplication
+ *Given a single instance of N in the above system and an integer m, write a program that prints out the value N^m (N*N*N m times)
+ *The first line has an integer T, with T data sets to follow.
+ *Each data set contains two decimal values a, and b, representing the ordered pair (a,b), followed by an integer M representing the exponent.
+ *For each test case, output the answer as an orderd pair, bracketed as shown, without spaces.
  */
 public class Exponentiation {
 	
