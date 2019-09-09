70d0ab579e39f531c0e5316ee692fd983d06dc11
pfenoglio <54938878+pfenoglio@users.noreply.github.com>
2019-09-09 18:13:00 -0500

    Update Rajev.java

--- Strings/Rajev/Rajev.java
+++ Strings/Rajev/Rajev.java
@@ -6,21 +6,39 @@ public class Rajev {
 	public static void main(String[] args) throws IOException {
 		Scanner scan = new Scanner(new File("rejeev.dat"));
 		while(scan.hasNextLine()) {
-			String str = scan.nextLine();
-			//System.out.println(str);
-			String[] arr = str.split(",");
-			//System.out.println(Arrays.toString(arr));
-			for(int i = 2; i<arr.length; i++) {
-				if(!arr[i].matches(".*uiltexas.org")&&!arr[i].matches(".*uil.net"))
-					System.out.println(arr[i]);
-				
-				
-				
-			}
 			
+			String str = scan.nextLine();
+			String[] arr = str.split(",");
+			//ArrayList to store Strings to print
+			ArrayList<String> list = new ArrayList();
+			//add the fisrt part of the line
+			list.add(arr[0]+":"+arr[1]);
+			//we will only print the line if we find an email that does not belong
+			boolean flag = false;
+			for(int i = 2; i<arr.length; i++) 
+				if(!arr[i].matches(".*uiltexas.org")&&!arr[i].matches(".*uil.net")) {
+					list.add(arr[i]);
+					 flag = true;
+				}
+			//Now to print the data
+			String ans="";
+			if(flag==true) {
+			for(String s: list)
+				ans+=":"+s;
+			//sub(1) to cut off the first :
+			System.out.println(ans.substring(1));
+			}
+		
+		
 		}
 		
-
+			
+		
+		
+ 
 	}
+
+}
+
 
 }
