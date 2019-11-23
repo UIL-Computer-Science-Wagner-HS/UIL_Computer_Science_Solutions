2b3411f58e3d5935fdd31eeedb63ea83275c3f9b
pfenoglio <54938878+pfenoglio@users.noreply.github.com>
2019-11-23 07:07:24 -0600

    Update All.java

--- Permutations/All.java
+++ Permutations/All.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.util.*;
 public class All {
-	static ArrayList<int[]> list = new ArrayList();
+	static ArrayList<int[]> list = new ArrayList();//keeps the possible selections as an int[] of 1's or zero's
+	//input an array, return list above.  In current config, do not need arr bout just arr.length
 	static ArrayList<int[]> tryAll(int[] arr){
 		int n = 2<<(arr.length-1);
 		for(int i=1; i<n+1; i++) {
@@ -29,6 +30,7 @@ public class All {
 		int[] arr = {1,1,2,2,2};
 		 ArrayList<int[]> list1 = tryAll(arr);
 		 int ans =0;
+		//this sums up all possible combinaions of arr an checks to see if they = 4.
 		 for (int[] a: list) {
 			 int sum =0;
 			 for(int i=0; i<arr.length; i++) {
