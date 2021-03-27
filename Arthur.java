c1a62d17cbbe34491a43b7121ed57e4fa6967178
Ian Fernandes <34047201+fernandesi2244@users.noreply.github.com>
2021-03-26 22:59:14 -0500

    Update Arthur.java

--- Arthur.java
+++ Arthur.java
@@ -17,70 +17,7 @@ public class Arthur {
 				System.out.printf("%d, %d, %d is a Pythagorean triple.%n", numbers.get(0), numbers.get(1), numbers.get(2));
 			} else {
 				System.out.printf("%d, %d, %d is not a Pythagorean triple.%n", numbers.get(0), numbers.get(1), numbers.get(2));
-			}
-			/**
-			ArrayList <Integer> nums = new ArrayList<Integer>();
-			nums.add(scan.nextInt());
-			nums.add(scan.nextInt());
-			nums.add(scan.nextInt());
-			nums.sort(null);
-		
-			if((nums.get(0)*nums.get(0) + nums.get(1)*nums.get(1)) == (nums.get(2)*nums.get(2))  ) {
-				System.out.print(nums.get(0) + ", " + nums.get(1)+ ", " + nums.get(2));
-				System.out.print(" is a Pythagorean triple");
-				//break;
-			}
-			else {
-				if(nums.get(0)+ nums.get(1) > nums.get(2)) {
-					System.out.print(nums.get(0) + ", " + nums.get(1)+ ", " + nums.get(2));
-					System.out.print(" is a Pythagorean triple");
-					//break;
-				}
-				else {
-					System.out.print(nums.get(0) + ", " + nums.get(1)+ ", " + nums.get(2));
-					System.out.print(" is not a Pythagorean triple");
-				}
-			}
-			System.out.println();
-			//System.out.println(nums);
-//			int x = scan.nextInt();
-//			int y = scan.nextInt();
-//			int z = scan.nextInt();
-//			scan.nextLine();
-//			int c = 0;
-//			boolean triple = true;
-//			if (x > y) {
-//				if(x > z) {
-//					c = x;
-//					if(y + z > c) {
-//						triple = false;
-//					}
-//				}
-//				else {
-//					c = z;
-//					if(y + x > c) {
-//						triple = false;
-//					}
-//				}
-//			}
-//			else {
-//				if(y > z) {
-//					c = y;
-//					if(z + x > c) {
-//						triple = false;
-//					}
-//				}
-//				else {
-//					c = z;
-//					if(y + x > c) {
-//						triple = false;
-//					}
-//				}
-//			}
- * 
- */
-			
-			
+			}		
 		}
 	}
 }
