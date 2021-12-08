7e0da53e08dfa8755aae597001d803a4bb38b773
pfenoglio <54938878+pfenoglio@users.noreply.github.com>
2021-12-08 09:18:20 -0600

    Add files via upload

---
+++ Gracie_Accelarted/SortByNewAlphabet.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class SortByNewAlphabet {
+	
+	static String newAlpha = "zxcvbnmasdfghjklqwertyuiop";
+static	Comparator<String> comp = (a,b)->	{
+		if(a.length()==0||b.length()==0)
+			return a.length()-b.length();
+		if(a.charAt(0)!=b.charAt(0))
+			return newAlpha.indexOf(a.charAt(0))-newAlpha.indexOf(b.charAt(0));
+		return SortByNewAlphabet.comp.compare(a.substring(1), b.substring(1));
+		
+};
+public static void main(String[] args) throws IOException{
+	//Scanner scan = new Scanner(new File(""));
+		//int noSets = scan.nextInt();
+	List<String> list = Arrays.asList("werwer", "zoo", "abc", "d");
+	System.out.println(list);
+	list.sort(comp);
+	System.out.println(list);
+	
+
+}
+}
