0f8b1713a7c6442eeb9a53f7ddbc35df5c4fb708
pfenoglio <54938878+pfenoglio@users.noreply.github.com>
2022-08-21 16:36:31 -0500

    Add files via upload

---
+++ Panjay.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Panjay {
+
+	static boolean good(List<String> left, List<String> right, Map<String, List<String>> map) {
+		for (String l : left) {
+			if (map.get(l) != null)
+				for (String s : map.get(l))
+					if (left.contains(s))
+						return false;
+
+		}
+		for (String l : right) {
+			if (map.get(l) != null)
+				for (String s : map.get(l))
+					if (right.contains(s))
+						return false;
+
+		}
+		//System.out.println(map);
+		//System.out.println(left);
+		//System.out.println(right);
+		return true;
+	}
+
+	public static void main(String... args) throws IOException {
+
+		Scanner scan = new Scanner(new File("panjay.dat"));
+		int noSets = scan.nextInt();
+		scan.nextLine();
+		int testCase = 1;
+		while (noSets-- > 0) {
+			
+			List<String> list = Arrays.asList(scan.nextLine().split(","));
+			//System.out.println("list "+list);
+			String[] haters = scan.nextLine().split(",|<->");
+			//System.out.println(Arrays.toString(haters));
+			Map<String, List<String>> map = new HashMap<>();
+			for (int i = 0; i < haters.length; i += 2) {
+				map.putIfAbsent(haters[i], new ArrayList<String>());
+				map.get(haters[i]).add(haters[i + 1]);
+				map.put(haters[i], map.get(haters[i]));
+				map.putIfAbsent(haters[i + 1], new ArrayList<String>());
+				map.get(haters[i + 1]).add(haters[i]);
+
+			}
+			//System.out.println(map);
+			boolean ans = false;
+			lab: for (int i = 0; i < 1 << list.size(); i++) {
+				List<String> right = new ArrayList<>();
+				List<String> left = new ArrayList<>();
+				for (int j = 0; j < list.size(); j++) {
+					if ((i >> j & 1) == 1)
+						right.add(list.get(j));
+					else
+						left.add(list.get(j));
+
+				}
+				if (good(right, left, map)) {
+					ans = true;
+					break lab;
+				}
+
+			}
+		
+			System.out.println("Test Case "+(testCase++) +": "+ (ans?"possible":
+				"impossible"));
+		}
+
+	}
+}
