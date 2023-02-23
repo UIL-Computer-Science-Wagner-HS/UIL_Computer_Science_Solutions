afc59c51b720f27697c6c51549a842b4409710f1
pfenoglio <54938878+pfenoglio@users.noreply.github.com>
2023-02-23 14:28:42 -0600

    Add files via upload

---
+++ to send/Rohit.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*; 
+public class Rohit {
+public static void main(String... args) throws IOException {
+	Scanner scan = new Scanner(new File("rohit.dat"));
+	int noSets = scan.nextInt();
+	scan.nextLine();
+	while (noSets-- > 0) {
+		
+
+	}
+
+}
+}
+/*
+4
+2 3 4
+4
+5 2 2
+23
+2 2 2 2 2 2 2
+119
+1 1 1 1 1 
+12
+
+
+
+
+
+
+
+*/
