0ddb399340b844c4e4690048cc5ff35bc8c82ded
pfenoglio <54938878+pfenoglio@users.noreply.github.com>
2023-05-07 14:23:40 -0500

    Add files via upload

---
+++ CSA2023FRQ/AppointmentBook.java
@@ -0,0 +1,34 @@
+
+public class AppointmentBook {
+
+	public int findFreeBlock(int period, int duration) {
+		for(int i = 0; i<=60-duration; i++) {
+			boolean flag = true;
+			for(int i = j; j<i+duration; j++) {
+				if(!isMinuteFree)
+					flag = false;
+			}
+			if(flag)
+				return i;
+			
+			
+		
+	}
+		return -1;
+		
+	}
+	
+	public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
+		for(int per = startPeriod, per<=endPeriod, per++) {
+			if(findFreeBlock(per,duration)!=-1) {
+				reserve(per, findFreeBlock(per, duration),duration);
+				return true;
+			}
+					
+		}
+		
+		return false;
+	}
+	
+	
+}
