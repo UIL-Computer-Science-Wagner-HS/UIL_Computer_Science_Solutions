2bd939d45b28c8dfc6b1318b447d20bde04292f3
Jack3266 <63890170+Jack3266@users.noreply.github.com>
2023-05-08 15:43:54 -0500

    Update AppointmentBook.java

--- CSA2023FRQ/AppointmentBook.java
+++ CSA2023FRQ/AppointmentBook.java
@@ -4,8 +4,8 @@ public class AppointmentBook {
 	public int findFreeBlock(int period, int duration) {
 		for(int i = 0; i<=60-duration; i++) {
 			boolean flag = true;
-			for(int i = j; j<i+duration; j++) {
-				if(!isMinuteFree)
+			for(int j = i; j<i+duration; j++) {
+				if(!isMinuteFree(period,j))
 					flag = false;
 			}
 			if(flag)
